849846d68a7391c27b3bfd72b92caf0f7e7a12f8
Agne Postnikova <N00211108@iadt.ie>
2023-12-06 16:02:52 +0000

    added materialize

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { AuthProvider } from "./contexts/AuthContext";
 import App from './App';
+import M from 'materialize-css'
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 		<AuthProvider>
-			<App />
+			<App/>
 		</AuthProvider>
   </React.StrictMode>
 );
